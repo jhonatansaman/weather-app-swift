020233070ba7c43f6f1b508749460dcfc6377bfe
Jhonatan Saman <jhonatan.samann@gmail.com>
2022-10-02 16:57:42 -0300

    add project

---
+++ WeatherSwift/WeatherSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherSwiftApp.swift
+//  WeatherSwift
+//
+//  Created by Jhonatan Saman on 02/10/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
